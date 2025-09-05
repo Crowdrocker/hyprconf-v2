a41f4ed0baa9a56a0939eb29bec74d1fc8099695
Matthew B. <berksapexlegends@gmail.com>
2025-09-05 13:42:45 -0500

    update

--- hyprconf-v2.sh
+++ hyprconf-v2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Advanced Hyprland Installation Script by
-# Shell Ninja ( https://github.com/shell-ninja )
+# Crowdrocker ( https://github.com/Crowdrocker )
 
 # color defination
 red="\e[1;31m"
